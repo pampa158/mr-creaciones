271c0d296ccb2ea4f6d0bf40ff075bfe56c8bee3
Ulises Nicola <martinnicola10@hotmail.com>
2025-09-23 21:27:08 -0300

    prototipo de modal y .btn responsive en tablet chica

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ window.addEventListener('scroll', function() {
         }
 
 });
+
+
+
+
+// Obtener los elementos
+const openModalBtn = document.getElementById("openModalBtn");
+const modal = document.getElementById("myModal");
+const closeButton = document.querySelector(".close-button");
+
+// Abrir el modal
+openModalBtn.addEventListener("click", () => {
+    modal.style.display = "flex"; // Usa 'flex' o 'block' para mostrarlo
+});
+
+// Cerrar el modal con el botón de cerrar
+closeButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Cerrar el modal al hacer clic fuera del contenido
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
